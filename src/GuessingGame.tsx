e408dfec556bfa118f2603062269f59f7a00b552
Thomas Oliver <tommy.oliver@live.com>
2023-12-30 13:37:24 -0500

    Minor lint cleanup, change category to dropdown

--- src/GuessingGame.tsx
+++ src/GuessingGame.tsx
@@ -1,5 +1,5 @@
 import WheelOfFortuneWall from './WheelOfFortuneWall';
-import React, { useEffect } from 'react';
+import React from 'react';
 import './GuessingGame.css'
 import useSound from 'use-sound';
 import rstlneSound from "./sounds/rstlne.mp3";
--- src/InfoGather.tsx
+++ src/InfoGather.tsx
@@ -14,10 +14,53 @@ const InfoGather: React.FunctionComponent<React.PropsWithChildren<InfoGatherProp
     onSubmit(category, phrase);
   }
 
+  const listOfCategories = [
+    "Around the House",
+    "Before & After",
+    "Best Seller",
+    "Character",
+    "Classic TV",
+    "College Life",
+    "Event",
+    "Family",
+    "Fun & Games",
+    "Food & Drink",
+    "Headline",
+    "Husband & Wife",
+    "In the Kitchen",
+    "Landmark",
+    "Living Thing",
+    "Movie Quote",
+    "TV Quote",
+    "Occupation",
+    "On the Map",
+    "Person",
+    "Phrase",
+    "Place",
+    "Proper Name",
+    "Quotation",
+    "Rhyme Time",
+    "Same Letter",
+    "Same Name",
+    "Show Biz",
+    "Song/Artist",
+    "Song Lyrics",
+    "Star & Role",
+    "Thing",
+    "Title",
+    "Title/Author",
+    "What Are You Doing?",
+    "What Are You Wearing?"
+  ];
+
   return (
     <form onSubmit={onInfoGatherSubmit}>
       <h2>Category</h2>
-      <input type="text" id="category" name="category" value={category} onChange={e => setCategory(e.target.value)}/>
+      <select name="category" value={category} onChange={e => setCategory(e.target.value)}>
+        {
+          listOfCategories.map(catName => <option value={catName}>{catName}</option>)
+        }
+      </select>
       <h2>Phrase</h2>
       <input type="text" id="phrase" name="phrase" value={phrase} onChange={e => setPhrase(e.target.value)}/>
       <button type='submit'>Submit</button>
